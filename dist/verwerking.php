bd647fa15312ff7c1aa9f3cd6aeb6d332c045c81
Pieter-janssen <r0755997@student.thomasmore.be>
2020-03-30 12:14:28 +0200

    phph

--- dist/verwerking.php
+++ dist/verwerking.php
@@ -5,21 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulierverwerking</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/stylish-portfolio.min.css">
+    <link rel="stylesheet" href="css/verwerking.css">
 </head>
 
 <body>
-<h1>Formulierverwerking</h1>
-<p>Niet alle formuliervelden worden hieronder nog eens getoond. We ontvingen wel alle informatie!</p>
+
+
+<header class="masthead d-flex">
+    <div class="container text-center my-auto">
+        <h1 class="mb-1">
+            <img src="img/logo-ybo-blauw-3.svg" alt="Logo YBO" title="YBO - Your Best Option" class="img-fluid">
+        </h1>
+        <h3 class="mb-5">Formulierverwerking</h3>
+        <p class="beschrijving">Hieronder wordt al de verkregen informatie getoont.</p>
+    </div>
+    <div class="overlay"></div>
+</header>
+
+
 <hr>
+<section class="bg-secondary">
 <?php
-if (isset($_POST["naam"]) && $_POST["naam"] != "") {
-    echo "<p> Ingevuld bij naam: " . $_POST["naam"] . "</p>\n";
+if (isset($_POST["Inputnaam"]) && $_POST["Inputnaam"] != "") {
+    echo "<p> Welkom, " . $_POST["Inputnaam"] . "</p>\n";
 } else {
     echo "<p> Gebruikersnaam werd niet ingevuld </p>\n";
 }
 
-if (isset($_POST["email"]) && $_POST["email"] != "") {
-    echo "<p> Ingevuld bij email: " . $_POST["email"] . "</p>\n";
+if (isset($_POST["InputEmail"]) && $_POST["InputEmail"] != "") {
+    echo "<p> Ingevuld bij email: " . $_POST["InputEmail"] . "</p>\n";
 } else {
     echo "<p> Email werd niet ingevuld </p>\n";
 }
@@ -39,7 +55,7 @@ if (isset($_POST["aanraden"]) && $_POST["aanraden"] != "") {
 
 
 ?>
-
+</section>
 </body>
 
 </html>
