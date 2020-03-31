352c8276e60b2c5cf4bfe196d0347bd93452b370
adnanechentouf <r0785947@student.thomasmore.be>
2020-03-31 16:05:29 +0200

    scriptvermelding

--- dist/verwerking.php
+++ dist/verwerking.php
@@ -129,6 +129,11 @@ if (isset($_POST["aanraden"]) && $_POST["aanraden"] != "") {
     </div>
 </footer>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+<script src="js/stylish-portfolio.min.js"></script>
+
 </body>
 
 </html>
